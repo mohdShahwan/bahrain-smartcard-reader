29209c14d105ff2bc04c4025e6e5d12fe3294fdf
mohdShahwan <mshahwan002@gmail.com>
2024-06-22 16:13:02 +0300

    Second binary selection

--- src/index.ts
+++ src/index.ts
@@ -6,162 +6,6 @@ const pcsclite = pcsc();
 let globalReader: any = null;
 let globalProtocol: any = null;
 
-async function readSmartcard1(reader, protocol) {
-  let smartcardData: any = {};
-  // Select DF by name
-  const selectApduCommand = Buffer.from([
-    0x00, 0xa4, 0x04, 0x00, 0x0d, 0xd4, 0x99, 0x00, 0x00, 0x01, 0x01, 0x01,
-    0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
-  ]);
-  await reader.transmit(selectApduCommand, 255, protocol, function (err, data) {
-    if (err) {
-      console.error("Error selecting application:", err);
-    } else {
-      console.log("Application selection response:", data.toString("hex"));
-      // Select DF file
-      const selectDfCommand = Buffer.from([
-        0x00, 0xa4, 0x00, 0x0c, 0x02, 0x01, 0x01,
-      ]);
-      reader.transmit(selectDfCommand, 255, protocol, (err, response) => {
-        if (err) {
-          console.error("Error selecting DF:", err);
-        } else {
-          // Select first data file
-          const selectDfCommand = Buffer.from([
-            0x00, 0xa4, 0x02, 0x0c, 0x02, 0x00, 0x01,
-          ]);
-          reader.transmit(selectDfCommand, 255, protocol, (err, response) => {
-            if (err) {
-              console.error("Error selecting first data file:", err);
-            } else {
-              // Read first record binary data
-              // Second record contains cprNumber, firstNameEn, middleName1En,
-              // middleName2En, middleName3En, middleName4En
-              // lastNameEn and firstNameAr
-              const readBinaryCommand = Buffer.from([
-                0x00, 0xb0, 0x00, 0x00, 0xff,
-              ]);
-              reader.transmit(
-                readBinaryCommand,
-                512,
-                protocol,
-                (err, response) => {
-                  if (err) {
-                    console.error("Error reading first binary data:", err);
-                  } else {
-                    const data = response.toString("utf8");
-                    smartcardData = {
-                      cprNumber:
-                        data.substring(0, 9).trim().length === 8
-                          ? "0" + data.substring(0, 9).trim()
-                          : data.substring(0, 9).trim(),
-                      firstNameEn: data.substring(9, 41).trim(),
-                      middleName1En: data.substring(41, 73).trim(),
-                      middleName2En: data.substring(73, 105).trim(),
-                      middleName3En: data.substring(105, 137).trim(),
-                      middleName4En: data.substring(137, 169).trim(),
-                      lastNameEn: data.substring(169, 201).trim(),
-                      firstNameAr: data.substring(201, data.length - 2).trim(),
-                    };
-                  }
-                }
-              );
-              // Read second record binary data
-              // Second record contains middleName1Ar, middleName2Ar,
-              // middleName3Ar, middleName4Ar and lastNameAr
-              const readBinaryCommand2 = Buffer.from([
-                0x00, 0xb0, 0x01, 0x00, 0xff,
-              ]);
-              reader.transmit(
-                readBinaryCommand2,
-                512,
-                protocol,
-                (err, response) => {
-                  if (err) {
-                    console.error("Error reading second binary data:", err);
-                  } else {
-                    data = response.toString("utf-8");
-                    const firstNameAr = (
-                      smartcardData.firstNameAr + data.substring(0, 9)
-                    ).trim();
-                    const middleName1Ar = data.substring(9, 68).trim();
-                    const middleName2Ar = data.substring(68, 104).trim();
-                    const middleName3Ar = data.substring(105, 137).trim();
-                    const middleName4Ar = data.substring(137, 169).trim();
-                    const lastNameAr = data.substring(169, 201).trim();
-                    const unknown = data.substring(201, data.length - 1).trim();
-                    smartcardData = {
-                      ...smartcardData,
-                      firstNameAr,
-                      middleName1Ar,
-                      middleName2Ar,
-                      middleName3Ar,
-                      middleName4Ar,
-                      lastNameAr,
-                      unknown,
-                    };
-                    console.log(smartcardData);
-                  }
-                }
-              );
-            }
-          });
-        }
-      });
-      // Select Second DF
-      const selectDfCommand2 = Buffer.from([
-        0x00, 0xa4, 0x00, 0x0c, 0x02, 0x00, 0x02,
-      ]);
-      reader.transmit(selectDfCommand2, 255, protocol, (err, response) => {
-        if (err) {
-          console.error("Error selecting second DF:", err);
-        } else {
-        }
-      });
-    }
-  });
-  return smartcardData;
-}
-
-// V1
-function readSmartcardV1(reader, protocol) {
-  // Select CPR Dedicated File
-  const selectCprDf = Buffer.from("00A404000BF000000078010001435052", "hex");
-  const cprDfSize = 11299;
-  reader.transmit(selectCprDf, cprDfSize, protocol, function (err, response) {
-    if (err) {
-      console.error("Error selecting CPR DF:", err);
-    } else {
-      // Select CPR First Elementary File
-      const selectCprEf1 = Buffer.from("80A40804020001", "hex");
-      const cprEf1Size = 610;
-      reader.transmit(
-        selectCprEf1,
-        cprEf1Size,
-        protocol,
-        function (err, response) {
-          if (err) {
-            console.error("Error selecting CPR first elementary file:", err);
-          } else {
-            /*
-            First elementary file will contain:
-            - Full name in arabic: 6 names * 64 byte each
-            - Blood group: 3 bytes
-            - CPR number: 9 bytes
-            - Date of birth: 8 bytes
-            - Full name in english: 6 names * 32 byte each
-            - Gender: 1 byte
-            - CPR expiry date: 8 bytes
-          */
-            const data = response.toString("utf8");
-            console.log(data);
-          }
-        }
-      );
-    }
-  });
-}
-
 // V2
 function readSmartcard(reader, protocol) {
   // Select application
@@ -212,18 +56,6 @@ function readSmartcard(reader, protocol) {
                       } else {
                         const data = response.toString("utf8");
 
-                        //   cprNumber:
-                        //   data.substring(0, 9).trim().length === 8
-                        //     ? "0" + data.substring(0, 9).trim()
-                        //     : data.substring(0, 9).trim(),
-                        // firstNameEn: data.substring(9, 41).trim(),
-                        // middleName1En: data.substring(41, 73).trim(),
-                        // middleName2En: data.substring(73, 105).trim(),
-                        // middleName3En: data.substring(105, 137).trim(),
-                        // middleName4En: data.substring(137, 169).trim(),
-                        // lastNameEn: data.substring(169, 201).trim(),
-                        // firstNameAr: data.substring(201, data.length - 2).trim(),
-
                         cpr1Data.cprNumber =
                           data.substring(0, 9).trim().length === 8
                             ? "0" + data.substring(0, 9).trim()
@@ -254,16 +86,33 @@ function readSmartcard(reader, protocol) {
                           middleNameEn4 ? middleNameEn4 + " " : ""
                         }${lastNameEn}`;
                         cpr1Data.firstNameAr = data
-                          .substring(201, data.length - 2)
+                          .substring(201, data.length)
                           .trim();
                       }
                     }
                   );
 
+                  // Select second record
                   const selectSecondRecord = Buffer.from("00B000FFFF", "hex");
-                  
+                  const secondRecordSize = 257;
+                  reader.transmit(
+                    selectSecondRecord,
+                    secondRecordSize,
+                    protocol,
+                    function (err, response) {
+                      const data = response.toString("utf8");
+                      console.log(data);
+                      let lock: boolean = true;
+                      while (lock) {
+                        if (cpr1Data.firstNameAr) {
+                          lock = false;
+                        }
+                      }
+                    }
+                  );
+
                   const selectThirdRecord = Buffer.from("00B001FE57", "hex");
-                  
+
                   // Testing
                   setTimeout(function () {
                     console.log(cpr1Data);
